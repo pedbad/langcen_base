58e944c62aada1305d1ff67efa5bec4720344d21
Pedram Badakhchani <ped@tech3.langcen.private.cam.ac.uk>
2025-10-03 14:50:34 +0100

    Integrate django-cotton: add dependency, enable in settings, update Tailwind sources

--- src/config/settings.py
+++ src/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Third-party apps
+    "django_cotton",
     # "rest_framework",
     # "tailwind",
     # "django_extensions",
